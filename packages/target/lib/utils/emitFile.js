3d674c88871a7b47004541fed678585bf4e40adc
elcarim5efil <elcarim5efil@gmail.com>
2018-11-08 16:15:42 +0800

    feat: forbid emitting same files

--- packages/target/lib/utils/emitFile.js
+++ packages/target/lib/utils/emitFile.js
@@ -1,5 +1,17 @@
+const fs = require( 'fs' )
 const toAsset = require( './toAsset' )
+const getMD5 = require( './md5' )
+
+const emitteds = {}
 
 module.exports = function emitFile( filepath, source, compilation ) {
+  const md5 = getMD5( filepath + source )
+  const emiited = emitteds[ filepath ]
+  
+  // don't emit the same files
+  if ( (emiited && emiited === md5) || fs.existsSync( filepath ) ) {
+    return
+  }
+  emitteds[ filepath ] = md5
   compilation.assets[ filepath ] = toAsset( source )
 }
