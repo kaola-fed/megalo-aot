24688f344d5627147b80c4148168601f1ec1612a
elcarim5efil <elcarim5efil@gmail.com>
2019-01-14 16:10:02 +0800

    fix(postcss-tag-selector-plugin): ignore keyframe #63

--- packages/target/lib/postcss-plugins/tag-selector-transform.js
+++ packages/target/lib/postcss-plugins/tag-selector-transform.js
@@ -8,18 +8,16 @@ module.exports = postcss.plugin( 'postcss-transform-tag', function ( ) {
 
   return function ( root, result ) {
     root.each(r => {
-      if ( r.type === 'atrule' && !/-keyframe/.test( r.name ) ) {
-        root.walkRules( rule => {
-          const selector = parser( selectors => {
-            selectors.walkTags( tag => {
-              // ignore orignial
-              if ( ignoreTags.indexOf( tag.value ) === -1 ) {
-                tag.value = `._${tag.value}`
-              }
-            } )
-          } ).processSync( rule.selector )
-          rule.selector = selector
-        } )
+      if ( r.type !== 'atrule' || !/-keyframe/.test( r.name ) ) {
+        const selector = parser( selectors => {
+          selectors.walkTags( tag => {
+            // ignore orignial
+            if ( ignoreTags.indexOf( tag.value ) === -1 ) {
+              tag.value = `._${tag.value}`
+            }
+          } )
+        } ).processSync( r.selector )
+        r.selector = selector
       }
     })
   }
