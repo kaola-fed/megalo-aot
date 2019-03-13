7cf837a36336af9f904d3a464972690d066b9424
陈诗文 <mosquito1403@163.com>
2019-03-13 11:37:31 +0800

    refactor:move resolve option change to MegaloPlugin

--- packages/target/lib/index.js
+++ packages/target/lib/index.js
@@ -1,5 +1,4 @@
 const webpack = require( 'webpack' )
-const chalk = require('chalk')
 const normalizeCompiler = require( './utils/normalizeCompiler' )
 
 function createMegaloTarget( options = {} ) {
@@ -11,26 +10,12 @@ function createMegaloTarget( options = {} ) {
     const FunctionModulePlugin = require( 'webpack/lib/FunctionModulePlugin' )
     const JsonpTemplatePlugin = require( 'webpack/lib/web/JsonpTemplatePlugin' )
     const LoaderTargetPlugin = webpack.LoaderTargetPlugin
-    const MultiPlatformResolver = require( './plugins/MultiPlatformResolver' )
     const MegaloPlugin = require( './plugins/MegaloPlugin' )
     const CopyHtmlparsePlugin = require( './plugins/CopyHtmlparsePlugin' )
     const FrameworkPlugins = [
       require( './frameworks/vue/plugin' ),
       require( './frameworks/regular/plugin' )
     ]
-
-    // add to webpack resolve.plugins in order to resolve multi-platform js module
-    !compiler.options.resolve.plugins && (compiler.options.resolve.plugins = []);
-    compiler.options.resolve.plugins.push(new MultiPlatformResolver(platform));
-    
-    // require multi-platform module like a directory
-    const mainFiles = ['index', `index.${platform}`, 'index.default'];
-    const extensions = ['.vue', '.js', '.json'];
-    
-    compiler.options.resolve.mainFiles = getConcatedArray(compiler.options.resolve.mainFiles, mainFiles);
-    compiler.options.resolve.extensions = getConcatedArray(compiler.options.resolve.extensions, extensions);
-
-    compiler.options.resolve.mainFiles.length > mainFiles.length && console.log(chalk.yellow('warning') + " megalo modified your webpack config " + chalk.bgBlue("resolve.mainFiles") + ", contact us if any problem occurred");
 
     new FunctionModulePlugin().apply( compiler )
     new JsonpTemplatePlugin().apply( compiler )
@@ -48,14 +33,6 @@ function normalizeOptions( options = {} ) {
   return Object.assign( {}, options, {
     compiler: normalizeCompiler( options.compiler || {} )
   } )
-}
-
-function getConcatedArray (source, target) {
-  if (!source) {
-    return target;
-  }
-
-  return [...new Set(source.concat(target))]
 }
 
 module.exports = createMegaloTarget
